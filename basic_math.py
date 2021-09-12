beb7e9be60a1c31b0ff37fd3fd152b8df01665fc
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-12 22:11:58 +0000

    basic math

---
+++ basic_math.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created by: Haokai Li
+# Created on: Sept 2021
+# This is the "bsic math"Program, with proper style
+
+
+def main():
+    print("a = 5 x 3 = {}".format(5 * 3))
+    print("p = (5 + 3) x 2 = {}".format((5 + 3) * 2))
+
+if __name__ == "__main__":
+    main()
+
